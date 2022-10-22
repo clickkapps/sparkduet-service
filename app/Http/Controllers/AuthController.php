8410646986336fe4783ecfdf045e6515bf3a6e70
Daniel Kwakye <dannykay.developer@gmail.com>
2022-10-22 03:09:40 +0000

    apple login added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Classes\ApiResponse;
-use App\Events\UserCreatedEvent;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -25,13 +24,18 @@ class AuthController extends Controller
         $provider = $request->get('provider');
         if($provider == "google") {
             return Socialite::driver('google')->redirect();
+        }else if ($provider == 'apple') {
+            return Socialite::driver("sign-in-with-apple")
+                ->scopes(["name", "email"])
+                ->redirect();
         }
 
         return response()->json(ApiResponse::failedResponse('Invalid provider'));
 
     }
 
-    public function oAuth2ProviderCallback(Request $request){
+    public function oAuth2ProviderCallback(Request $request): \Illuminate\Routing\Redirector|\Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse
+    {
 
         try {
 
@@ -45,35 +49,45 @@ class AuthController extends Controller
             }
 
             $provider = $request->get('provider');
+
+            $email = null;
+
             if($provider == 'google') {
                 $user = Socialite::driver('google')->user();
-                Log::info('returned social user: ' . json_encode($user));
-
+                Log::info('returned google user: ' . json_encode($user));
                 $email = $user->getEmail();
-                if(blank($email)){
-                    abort(400);
-                }
 
-                $parts = explode('@', $email);
-                $name = $parts[0];
-
-                // authenticate user with provider email and then redirect to url with the user token
-                $user = User::firstOrCreate([
-                    'email' => $email
-                ], [
-                    'name' => $name,
-                    'last_login_at' => now()
-                ]);
-
-                $token = $user->createToken($email);
-                Log::info('token: ' . json_encode($token));
-
-                return redirect('/?token=' . $token->plainTextToken);
-
+            }else if ($provider == 'apple') {
+                // get abstract user object, not persisted
+                $user = Socialite::driver("sign-in-with-apple")
+                    ->user();
+                Log::info('returned apple user: ' . json_encode($user));
+                $email = $user->getEmail();
             }
 
 
-            abort(400);
+            // once the email is not null continue
+
+            if(blank($email)){
+                abort(400);
+            }
+
+            $parts = explode('@', $email);
+            $name = $parts[0];
+
+            // authenticate user with provider email and then redirect to url with the user token
+            $user = User::firstOrCreate([
+                'email' => $email
+            ], [
+                'name' => $name,
+                'last_login_at' => now()
+            ]);
+
+            $token = $user->createToken($email);
+            Log::info('token: ' . json_encode($token));
+
+            return redirect('/?token=' . $token->plainTextToken);
+
 
 
         } catch (\Exception $e) {
