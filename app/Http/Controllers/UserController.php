e27d2c8f312bd8c0df34b46439086857eaf8d260
Daniel Kwakye <dannykay.developer@gmail.com>
2022-10-24 17:45:36 +0000

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,17 +11,17 @@ class UserController extends Controller
     public function getUserProfile(Request $request, $id = null): \Illuminate\Http\JsonResponse
     {
         // if userId is null get the authenticated user profile
-        $userId = $request->user();
+        $userId = $request->user()->id;
         if(!blank($id)){
             $userId = $id;
         }
 
         $user = User::find($userId);
-        $user->description = "";
 //        if($user->info) {
 //
 //        }
 
+//        return response()->json(ApiResponse::successResponseV2($user));
         return response()->json(ApiResponse::successResponseV2($user));
 
     }
