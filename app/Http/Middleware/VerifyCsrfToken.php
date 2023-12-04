ca377c3eeb12fb6d9539dd8edd8efadc8626a0b9
Daniel Kwakye <dannykay.developer@gmail.com>
2023-12-04 16:15:08 +0000

    added socialit

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/auth/social/callback?provider=apple',
+        'https://sparkduet.com/auth/social/callback?provider=apple',
     ];
 }
