76f4617202c27f08ed62bdc0d39cf3a6ccc3fe24
Daniel K <daniel@techvision.global>
2024-05-18 05:19:48 +0000

    wip: added mix plugin

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Classes\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
-use JetBrains\PhpStorm\ArrayShape;
 use MuxPhp\Configuration;
 use MuxPhp\Api\DirectUploadsApi;
 use GuzzleHttp\Client;
@@ -43,18 +43,25 @@ class UtilsController extends Controller
 
     public function getMuxUploadUrl(Request $request): \Illuminate\Http\JsonResponse
     {
+        $muxTokenId = config('custom.mux_token_id');
+        $muxTokenSecret = config('custom.mux_token_secret');
+        $appUrl = config('app.url');
+
+        Log::info("muxTokenId: $muxTokenId");
+        Log::info("muxTokenSecret: $muxTokenSecret");
+        Log::info("appUrl: $appUrl");
 
         // Authentication Setup
         $config = Configuration::getDefaultConfiguration()
-            ->setUsername(getenv('MUX_TOKEN_ID'))
-            ->setPassword(getenv('MUX_TOKEN_SECRET'));
+            ->setUsername($muxTokenId)
+            ->setPassword($muxTokenSecret);
 
         $uploadsApi = new DirectUploadsApi(
             new Client(),
             $config
         );
 
-        $appUrl = config('app.url');
+
         $createAssetRequest = new CreateAssetRequest(["playback_policy" => [PlaybackPolicy::_PUBLIC], "encoding_tier" => "baseline"]);
         $createUploadRequest = new CreateUploadRequest(["timeout" => 3600, "new_asset_settings" => $createAssetRequest, "cors_origin" => $appUrl]);
         $upload = $uploadsApi->createDirectUpload($createUploadRequest);
