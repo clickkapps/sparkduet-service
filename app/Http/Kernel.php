4bc203598fb28cc703f9a5f2e74459cc75be3270
Daniel K <daniel@techvision.global>
2024-06-11 08:53:32 +0000

    wip: added admin login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'basicAuth' => \App\Http\Middleware\BasicAuth::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class
     ];
 }
