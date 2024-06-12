db5b9f848931a01fa779ab6bcc2b5152f5321148
Daniel K <daniel@techvision.global>
2024-06-12 20:28:54 +0000

    wip: revcat integration

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,9 +12,9 @@ class PaymentController extends Controller
     {
         $event = $request->all()['event'];
         json_encode("Revcat callback payload => " . json_encode($event));
-        $productId = $event['product_id'];
-        $appUserId = $event['app_user_id'];
-        $type = $event['type'];
+        $productId = $event['product_id'] ?? "";
+        $appUserId = $event['app_user_id'] ?? "";
+        $type = $event['type'] ?? "";
         $admin = getAdmin();
         if(!blank($admin)) {
             $admin->notify(new SubscriptionUpdated(appUserId: $appUserId, type: $type, productId: $productId));
