129a7c31098395f4ba140f1898b79bcb30704078
Daniel K <daniel@techvision.global>
2024-06-12 18:27:39 +0000

    wip: revcat integration

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,8 +9,8 @@ class PaymentController extends Controller
     public function revenueCatWebhookCallback(Request $request): \Illuminate\Http\JsonResponse
     {
         $event = $request->all()['event'];
-        $revCatAppUserId = $event->payload['app_user_id'];
-        json_encode("Revcat callback payload => " . json_encode($event->payload));
+//        $revCatAppUserId = $event->payload['app_user_id'];
+        json_encode("Revcat callback payload => " . json_encode($event));
         return response()->json();
     }
 }
