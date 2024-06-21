03506e98274b1a06258ceeb9bcce0c1f6b28ed7b
Daniel K <daniel@techvision.global>
2024-06-21 04:59:58 +0000

    encrypted chat messages before they are stored

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class ChatMessage extends Model
 {
@@ -27,5 +28,27 @@ class ChatMessage extends Model
 
     public function parent(): \Illuminate\Database\Eloquent\Relations\BelongsTo {
         return $this->belongsTo(ChatMessage::class,'parent_id', 'id');
+    }
+
+    /**
+     * Encrypt the chat message before saving to the database.
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setTextAttribute(string $value): void
+    {
+        $this->attributes['text'] = Crypt::encryptString($value);
+    }
+
+    /**
+     * Decrypt the chat message when retrieving from the database.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getTextAttribute(string $value): string
+    {
+        return Crypt::decryptString($value);
     }
 }
