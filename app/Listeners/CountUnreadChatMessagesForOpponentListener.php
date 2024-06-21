55f8307fa3966c4bc201c2e5e8f45aba06a5f63b
Daniel K <daniel@techvision.global>
2024-06-21 10:52:26 +0000

    queued chat created listeners

--- app/Listeners/CountUnreadChatMessagesForOpponentListener.php
+++ app/Listeners/CountUnreadChatMessagesForOpponentListener.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class CountUnreadChatMessagesForOpponentListener
+class CountUnreadChatMessagesForOpponentListener implements ShouldQueue
 {
     /**
      * Create the event listener.
--- app/Listeners/SendPushToChatOpponentListener.php
+++ app/Listeners/SendPushToChatOpponentListener.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 
-class SendPushToChatOpponentListener
+class SendPushToChatOpponentListener implements ShouldQueue
 {
     /**
      * Create the event listener.
