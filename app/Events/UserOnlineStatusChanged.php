45705123fb46fadfc694c06d8e7c4c97969bd84b
Daniel K <daniel@techvision.global>
2024-06-27 07:10:03 +0000

    wip users-online

--- app/Events/UserOnlineStatusChanged.php
+++ app/Events/UserOnlineStatusChanged.php
@@ -19,7 +19,7 @@ class UserOnlineStatusChanged implements ShouldBroadcast
      *
      * @return void
      */
-    public function __construct(public $user, string $status)
+    public function __construct(public $user, public string $status)
     {
         //
     }
