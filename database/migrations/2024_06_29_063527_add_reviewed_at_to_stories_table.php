fa5b3c4c74dddd0cfd60427b9b5c8902c474298e
Daniel K <daniel@techvision.global>
2024-06-29 06:36:53 +0000

    added admin stories review

---
+++ database/migrations/2024_06_29_063527_add_reviewed_at_to_stories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->dateTime("reviewed_at")->nullable()->default(null);
+            $table->foreignId('reviewed_by')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->dropColumn(['reviewed_at', 'reviewed_by']);
+        });
+    }
+};
